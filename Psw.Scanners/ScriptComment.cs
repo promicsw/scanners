979199c15a7555f49d320f2715ffb29e0bb31f3c
Andre <apretorius@acenet.co.za>
2023-10-27 15:43:17 +0200

    Start ScriptComment

---
+++ Psw.Scanners/ScriptComment.cs
@@ -0,0 +1,23 @@
+﻿// -----------------------------------------------------------------------------
+// Copyright (c) 2023 Promic Software. All rights reserved.
+// Licensed under the MIT License (MIT).
+// -----------------------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Psw.Scanners
+{
+    internal class ScriptComment
+    {
+        public string LineComment = "//";
+        public string BlockCommentStart = "/*";
+        public string BlockCommentEnd = "*/";
+
+        // ToDo: comments may be empty?
+        public string CommentStartChar => LineComment[0] == BlockCommentStart[0] ? LineComment[0].ToString() : $"{LineComment[0]}{BlockCommentStart[0]}";
+    }
+}
